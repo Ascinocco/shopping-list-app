4e50fdf34c3a3f42eebef2a1d6d46a2ad58fe3d0
Anthony Scinocco <scinocco.a@gmail.com>
2017-02-22 08:51:02 -0500

    added create method for shopping list

--- src/pages/shoppingList/shoppingListComponent.ts
+++ src/pages/shoppingList/shoppingListComponent.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'shopping-list',
   templateUrl: 'shoppingListComponent.html'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent
+{
+  public shoppingLists:         FirebaseListObservable<any[]>
+  public newShoppingListTitle:  string;
+  public newShoppingListItem:   string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, af: AngularFire)
+  {
+    this.shoppingLists        = af.database.list('/shoppingLists');
+    this.newShoppingListTitle = "";
+    this.newShoppingListItem  = "";
+  }
+
+  /** ******************** Shopping list crud ******************** **/
+
+  public createShoppingList()
+  {
+    if (this.newShoppingListTitle.length > 0) {
+      let newShoppingList = {
+        title: this.newShoppingListTitle,
+        items: []
+      }
+
+      this.shoppingLists.push(newShoppingList);
+      this.newShoppingListTitle = "";
+
+    } else {
+      alert("Your shopping list must have a name!");
+    }
+  }
+
+  public updateShoppingList()
+  {
 
   }
 
+  public deleteShoppingList()
+  {
+
+  }
 }
